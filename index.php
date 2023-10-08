a38092bde07a8cfa35bcb81c7f0e672475b53380
xiaofan <fanzh610@163.com>
2023-10-08 17:08:39 +0800

    忽略文件

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
 
 
+$GLOBALS['C']['UrlRewrite']=1;
+$GLOBALS['C']['Debug']=0;
+$GLOBALS['C']['AdminDir']='admin';
+$GLOBALS['C']['SiteHash']='9e4f7581d5e7436d';
+$GLOBALS['C']['DbInfo']=array('host'=>'127.0.0.1','dbname'=>'liuyan','user'=>'liuyan','password'=>'DBTnni3EGEZLXTsb','prefix'=>'xf_','engine'=>'InnoDB','charset'=>'utf8mb4','kind'=>'mysqlpdo',);
+
+
 require('class/cms/cms.php');
 ClassCMS_init();
