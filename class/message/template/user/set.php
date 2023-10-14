e493d8c11f473de81fbb892e61b54bcaaf7e7752
xiaofan <fanzh610@163.com>
2023-10-14 10:00:42 +0800

    传息配置页优化

--- class/message/template/user/set.php
+++ class/message/template/user/set.php
@@ -1,59 +1,66 @@
-<?php if(!defined('ClassCms')) {exit();}?>
+<?php if (!defined('ClassCms')) {
+    exit();
+} ?>
 <!DOCTYPE html>
 <html>
 <head>{admin:head(设置)}</head>
 <body>
 <div class="layui-fluid  layui-form">
-<input type="hidden" name="_sender" value="{$sender.hash}">
+    <input type="hidden" name="_sender" value="{$sender.hash}">
     <div class="layui-row">
         <div class="layui-card">
             <div class="layui-card-body" style="padding-bottom:50px">
                 {loop $configs as $config}
-                    <div class="layui-form-item">
-                        <label class="layui-form-label">{$config.configname}</label>
-                        <div class="layui-input-right">
+                <div class="layui-form-item">
+                    <label class="layui-form-label">{$config.configname}</label>
+                    <div class="layui-input-right">
                         <div class="layui-input-block">
                             {cms:input:form($config)}
                         </div>
                         <div class="layui-form-mid">{$config.tips}</div>
-                        </div>
                     </div>
+                    {$config.qt}
+                </div>
                 {/loop}
-          </div>
-          
+            </div>
+
         </div>
         <div class="layui-form-item layui-layout-admin">
             <div class="layui-input-block">
                 <div class="layui-footer">
-                <button class="layui-btn cms-btn" lay-submit="" lay-filter="form-submit">保存</button>
-                <button type="button" class="layui-btn layui-btn-primary closebtn">关闭</button>            </div>
+                    <button class="layui-btn cms-btn" lay-submit="" lay-filter="form-submit">保存</button>
+                    <button type="button" class="layui-btn layui-btn-primary closebtn">关闭</button>
+                </div>
             </div>
-          </div>
-     </div>
+        </div>
+    </div>
 </div>
 <script>
-layui.use(['index'],function(){
-    layui.$('.tips').click(function(){
-        layer.tips(layui.$(this).attr('rel'),layui.$(this));
+    layui.use(['index'], function () {
+        layui.$('.tips').click(function () {
+            layer.tips(layui.$(this).attr('rel'), layui.$(this));
+        });
+        layui.form.on('submit(form-submit)', function (data) {
+            layui.$('button[lay-filter=form-submit]').blur();
+            layui.admin.req({
+                type: 'post', url: "?do={this}:user:setPost", data: data.field, async: true, beforeSend: function () {
+                    layui.admin.load('保存中...');
+                }, done: function (res) {
+                    if (res.error == 0) {
+                        var confirm = layer.confirm(res.msg, {btn: ['好的', '返回'], shadeClose: 1}, function () {
+                            layui.admin.events.reload();
+                        }, function () {
+                            parent.layer.close(parent.layer.getFrameIndex(window.name));
+                        });
+                    }
+                }
+            });
+            return false;
+        });
+        layui.$('.closebtn').click(function () {
+            parent.layer.close(parent.layer.getFrameIndex(window.name));
+        });
     });
-    layui.form.on('submit(form-submit)', function(data){
-        layui.$('button[lay-filter=form-submit]').blur();
-        layui.admin.req({type:'post',url:"?do={this}:user:setPost",data:data.field,async:true,beforeSend:function(){
-            layui.admin.load('保存中...');
-        },done: function(res){
-            if (res.error==0)
-            {
-                var confirm=layer.confirm(res.msg, {btn: ['好的','返回'],shadeClose:1},function(){layui.admin.events.reload();},function(){
-                     parent.layer.close(parent.layer.getFrameIndex(window.name));
-                 });
-            }
-        }});
-      return false;
-    });
-    layui.$('.closebtn').click(function(){
-        parent.layer.close(parent.layer.getFrameIndex(window.name));
-    });
-});
 </script>
 {admin:body:~()}
 </body>
--- class/message_chuanxi/message_chuanxi.php
+++ class/message_chuanxi/message_chuanxi.php
@@ -1,27 +1,36 @@
 <?php
-if(!defined('ClassCms')) {exit();}
-class message_chuanxi {
-    function hook() {
-        $hooks=array();
-        $hooks[]=array('hookname'=>'all','hookedfunction'=>'message:sender:all:=','enabled'=>1);
-        Return $hooks;
+if (!defined('ClassCms')) {
+    exit();
+}
+
+class message_chuanxi
+{
+    function hook()
+    {
+        $hooks = array();
+        $hooks[] = array('hookname' => 'all', 'hookedfunction' => 'message:sender:all:=', 'enabled' => 1);
+        return $hooks;
     }
-    function all($class,$args,$return){
-        $return[]=array(
-            'title'=>'传息',
-            'hash'=>'chuanxi',
-            'classhash'=>I(),
-            'classfunction'=>I().':sender',
-            'userconfig'=>array(
-                                array('configname'=>'appKey','hash'=>'appkey','inputhash'=>'text','tips'=>'<a href="https://cx.qingsonge.com/#" class="layui-btn">前往登录</a> <a style="color: red">获取appKey</a><br><iframe src="https://cx.qingsonge.com/#" width="600px" height="500px"></iframe>','defaultvalue'=>''),
+
+    function all($class, $args, $return)
+    {
+        $return[] = array(
+            'title' => '传息',
+            'hash' => 'chuanxi',
+            'classhash' => I(),
+            'classfunction' => I() . ':sender',
+            'userconfig' => array(
+                array('configname' => 'appKey', 'hash' => 'appkey', 'inputhash' => 'text', 'tips' => '<a href="https://cx.qingsonge.com/#" class="layui-btn">前往登录</a> <a style="color: red">获取appKey</a>', 'defaultvalue' => '','qt'=>'<iframe src="https://cx.qingsonge.com/#" width="100%" height="300px" frameborder="0"></iframe>'),
             )
         );
         return $return;
     }
-    function sender($config=array()) {
-        if(isset($config['appkey']) && $config['appkey']){
-            $config=array('title'=>$config['message']['title'],'appkey'=>$config['appkey'],'content'=>$config['message']['content']);
-            Return C('chuanxi:send',$config);
+
+    function sender($config = array())
+    {
+        if (isset($config['appkey']) && $config['appkey']) {
+            $config = array('title' => $config['message']['title'], 'appkey' => $config['appkey'], 'content' => $config['message']['content']);
+            return C('chuanxi:send', $config);
         }
         return false;
     }
